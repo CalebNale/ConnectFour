aa30e75565236f11b689e83d1ceaa8c9bd78bbf3
evrenke <evren.keskin@sjsu.edu>
2020-12-15 19:23:36 -0800

    Add files via upload

--- Minimax.py
+++ Minimax.py
@@ -5,6 +5,7 @@ class Minimax:
 
     def __init__(self, depth):
         self.depth = depth  # the maximum depth of the minimax algorithm
+
     def minimax(self, curr_depth, max_turn, state, player, alpha=-float('inf'), beta=float('inf') ):
         if curr_depth == self.depth or AIEnvironment.terminal(state):
             return AIEnvironment.evaluation_function(state, player), ""
@@ -17,10 +18,6 @@ class Minimax:
             if player == 'H':
                 nextplayer = 'A'
             eval_val, eval_action = self.minimax(curr_depth+1, not max_turn, new_state, nextplayer)
-            if best_val == -1000 :
-                print("best val: %d", best_val)
-            if eval_val == - 1000:
-                print("eval val: %d", eval_val)
             if max_turn and best_val < eval_val:
                 best_val = eval_val
                 best_action = action
